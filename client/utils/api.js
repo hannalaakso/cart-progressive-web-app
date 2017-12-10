4def4b39ce53d46ccf239db2c0bac0a4ec8739ee
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2017-12-10 22:02:04 +0000

    Allow debugging in Android simulator
    A temp fix to fix the API endpoint path to point to 10.0.2.2

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,5 +1,6 @@
 let metaContents = document.head.querySelector('meta[name="fegrocer-api-endpoint"]').content;
-let apiEndpoint = new URL(metaContents);
-apiEndpoint.protocol = window.location.protocol;
+//let apiEndpoint = new URL(metaContents);
+//apiEndpoint.protocol = window.location.protocol;
+let apiEndpoint = 'https://10.0.2.2:3100/'; // debug in Android simulator
 
 export const endpoint = apiEndpoint.toString();
